22e619512407311411a3db3d1c1227490c86fce0
Jordan Marine <marine.jordan64@proton.me>
2026-02-13 17:55:08 -0600

    Suppress error and fix tab spacing for KeyValueSidebarComponent.

--- src/main/java/me/kelpdo/faharescoreboard/scoreboard/KeyValueSidebarComponent.java
+++ src/main/java/me/kelpdo/faharescoreboard/scoreboard/KeyValueSidebarComponent.java
@@ -4,29 +4,31 @@ import java.util.function.Supplier;
 
 import org.jetbrains.annotations.NotNull;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.megavex.scoreboardlibrary.api.sidebar.component.LineDrawable;
 import net.megavex.scoreboardlibrary.api.sidebar.component.SidebarComponent;
 
 public class KeyValueSidebarComponent implements SidebarComponent {
-  private final Component key;
-  private final Supplier<Component> valueSupplier;
+    private final Component key;
+    private final Supplier<Component> valueSupplier;
 
-  public KeyValueSidebarComponent(@NotNull Component key, @NotNull Supplier<Component> valueSupplier) {
-    this.key = key;
-    this.valueSupplier = valueSupplier;
-  }
+    @SuppressFBWarnings(value = "EI2")
+    public KeyValueSidebarComponent(@NotNull Component key, @NotNull Supplier<Component> valueSupplier) {
+        this.key = key;
+        this.valueSupplier = valueSupplier;
+    }
 
-  @Override
-  public void draw(@NotNull LineDrawable drawable) {
-    var value = valueSupplier.get();
-    var line = Component.text()
-      .append(key)
-      .append(Component.text(": "))
-      .append(value.colorIfAbsent(NamedTextColor.AQUA))
-      .build();
+    @Override
+    public void draw(@NotNull LineDrawable drawable) {
+        var value = valueSupplier.get();
+        var line = Component.text()
+            .append(key)
+            .append(Component.text(": "))
+            .append(value.colorIfAbsent(NamedTextColor.AQUA))
+            .build();
 
-    drawable.drawLine(line);
-  }
+        drawable.drawLine(line);
+    }
 }
